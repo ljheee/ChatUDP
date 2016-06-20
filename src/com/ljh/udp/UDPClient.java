c98d9e086d5be7872498efe5232f546792586a5b
ljheee <3116466300@qq.com>
2016-06-20 14:32:37 +0800

    Add a little annotation.

--- src/com/ljh/udp/UDPClient.java
+++ src/com/ljh/udp/UDPClient.java
@@ -7,6 +7,7 @@ import java.net.*;
 /**
  * 给server发送一个，接收一个（数据包）就关闭。
  * @author ljheee
+ * https://github.com/ljheee/ChatUDP
  *
  */
 public class UDPClient {
@@ -16,21 +17,21 @@ public class UDPClient {
 		DatagramSocket clientSocket = null;
 		try {
 			//发送数据
-			clientSocket =new DatagramSocket();
+			clientSocket =new DatagramSocket();//构造“邮局”
 			InetAddress address = InetAddress.getByName("localhost");
 			byte[] receive = new byte[1024];
 			byte[] sendData = new byte[1024];
 			
 			String data = br.readLine();
 			sendData =data.getBytes();
-			//构建一个数据包，发送到address的指定端口
+			//构建一个数据包[一个封装好的信封]，发送到address的指定端口
 			DatagramPacket dp = new DatagramPacket(sendData, sendData.length,address,8899);
 			clientSocket.send(dp);
 			
-			//接受数据
+			//接收数据，提前准备一个空信封DatagramPacket
 			DatagramPacket dp1 = new DatagramPacket(receive, receive.length);
 			
-			clientSocket.receive(dp1);//接收数据，放到dp1数据包
+			clientSocket.receive(dp1);//接收数据，封装放到dp1数据包
 			String data1 = new String(dp1.getData());
 			System.out.println("Client's receive:"+data1);
 			
--- src/com/ljh/udp/UDPServer.java
+++ src/com/ljh/udp/UDPServer.java
@@ -8,6 +8,7 @@ import java.net.*;
  * 循环 监听端口
  * 接收到一个，打印输出接收到的内容，并返回发送一个消息
  * @author ljheee
+ * https://github.com/ljheee/ChatUDP
  *
  */
 public class UDPServer {
@@ -15,7 +16,7 @@ public class UDPServer {
 	public static void main(String[] args) {
 		DatagramSocket serSocket =null;
 		try {
-			serSocket = new DatagramSocket(8899);
+			serSocket = new DatagramSocket(8899);//构造“邮局”
 			
 			byte[] receive = new byte[1024];
 			byte[] sendData = new byte[1024];
@@ -24,7 +25,7 @@ public class UDPServer {
 				//构建一个数据包；里面没有数据--相当于“空信封”（准备接收数据）
 				DatagramPacket dp = new DatagramPacket(receive, receive.length);
 				
-				serSocket.receive(dp);//接收数据，放到dp数据包
+				serSocket.receive(dp);//接收数据，放到dp数据包，即把收到的数据封装到“空信封”里
 				String data = new String(dp.getData());
 				System.out.println("Server's receive:"+data);
 				
@@ -33,10 +34,12 @@ public class UDPServer {
 				int port = dp.getPort();
 				
 				BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-				String data1 = br.readLine();
+				String data1 = br.readLine();//从控制台--读取一行待发数据
 				sendData = data1.getBytes();
+				
+				//构造一封信DatagramPacket，信封里面封装了  待发数据、数据长、以及接收者信息
 				DatagramPacket dp1 = new DatagramPacket(sendData, sendData.length,address,port);
-				serSocket.send(dp1);//发送数据报文dp到目的地
+				serSocket.send(dp1);//发送数据报文dp1,到目的地
 				dp = null;
 			}
 			
